4a499333cbc27e18963df23c0d51b6f38b5cdcea
Gaowater <3384357097@qq.com>
2026-05-21 13:57:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -41,5 +41,5 @@ const cardsData = [
     }
 ];
 
-// 你的心情预设（可选：yuyue / youxian / qidai / ziyou / beishang / gudu / fennv / yihan / wuliao / kongxu / fadian /）
+// 你的心情预设（可选：yuyue / youxian / qidai / ziyou / beishang / gudu / fennu / yihan / wuliao / kongxu / fadian /）
 const myMood = "gudu";
